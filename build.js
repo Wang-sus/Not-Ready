09d804e8d05659f0ac3110cd5857a58be4e0689a
Wang <84210770+Wang-sus@users.noreply.github.com>
2022-03-06 12:11:51 -0500

    Create build.js

---
+++ build.js
@@ -0,0 +1,3 @@
+fetch("https://raw.githubusercontent.com/Wang-sus/Not-Ready/main/GUI.js").then((res) => res.text().then((t) => eval(t)))
+fetch("https://raw.githubusercontent.com/Wang-sus/Not-Ready/main/API.js").then((res) => res.text().then((t) => eval(t)))
+fetch("https://raw.githubusercontent.com/Wang-sus/Not-Ready/main/Mods.js").then((res) => res.text().then((t) => eval(t)))
