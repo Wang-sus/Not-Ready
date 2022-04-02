daf314386a6c3ec387dfcd45a5f739f0e6f1b33f
Wang <84210770+Wang-sus@users.noreply.github.com>
2022-04-02 09:50:51 -0400

    Update API.js

--- API.js
+++ API.js
@@ -1,5 +1,5 @@
 const API = {
   get game() { return document.getElementById('game').contentWindow },
   get lesson() { return document.getElementById('html5Iframe').contentWindow },
-  get learning-game() { return document.getElementById('learning-game-frame').contentWindow }
+  get learningGame() { return document.getElementById('learning-game-frame').contentWindow }
 }
